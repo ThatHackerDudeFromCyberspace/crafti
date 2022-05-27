4d29b97275b89bf0705b5dec3729abe70d8199b5
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-27 18:39:09 +0100

    CAKE

--- cactusrenderer.h
+++ cactusrenderer.h
@@ -22,7 +22,7 @@ public:
 
 protected:
     static constexpr GLFix cactus_height = BLOCK_SIZE;
-    static constexpr GLFix cactus_width = BLOCK_SIZE / 16 * 15;
+    static constexpr GLFix cactus_width = BLOCK_SIZE / 16 * 14;
 
 };
 
