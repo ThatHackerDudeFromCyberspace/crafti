99a512105e26750252ac3a5d834bf35855323e0e
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-28 17:37:24 +0100

    Refactor code

--- woolrenderer.h
+++ woolrenderer.h
@@ -15,6 +15,8 @@ public:
     virtual const char* getName(const BLOCK_WDATA) override;
 
 protected:
+    const TerrainAtlasEntry &getWoolTexture(const BLOCK_WDATA block);
+
     enum COLOUR {
         WHITE_WOOL=0, // white
         ORANGE_WOOL=1, // orange
