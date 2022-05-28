6842bae433cbe79fd025613e92fd0420577bce2c
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-28 15:10:44 +0100

    Add cactus renderer

---
+++ cactusrenderer.h
@@ -0,0 +1,28 @@
+#ifndef CACTUSRENDERER_H
+#define CACTUSRENDERER_H
+
+#include "blockrenderer.h"
+
+class CactusRenderer : public DumbBlockRenderer
+{
+public:
+    virtual void renderSpecialBlock(const BLOCK_WDATA, GLFix x, GLFix y, GLFix z, Chunk &c) override;
+    virtual void geometryNormalBlock(const BLOCK_WDATA, const int local_x, const int local_y, const int local_z, const BLOCK_SIDE side, Chunk &c) override;
+    virtual bool isOpaque(const BLOCK_WDATA /*block*/) override { return false; }
+    virtual bool isObstacle(const BLOCK_WDATA /*block*/) override { return true; }
+    virtual bool isOriented(const BLOCK_WDATA /*block*/) override { return true; } // Oriented
+    virtual bool isFullyOriented(const BLOCK_WDATA /*block*/) override { return false; } // Torch-like orientation
+
+    virtual bool isBlockShaped(const BLOCK_WDATA /*block*/) override { return false; }
+    virtual AABB getAABB(const BLOCK_WDATA, GLFix x, GLFix y, GLFix z) override;
+
+    virtual void drawPreview(const BLOCK_WDATA block, TEXTURE &dest, int x, int y) override;
+    
+    virtual const char* getName(const BLOCK_WDATA) override;
+
+protected:
+    static constexpr GLFix cactus_height = BLOCK_SIZE / 16 * 9;
+    static constexpr GLFix cactus_width = BLOCK_SIZE / 16 * 15;
+};
+
+#endif // CACTUSRENDERER_H
