99415d8808dfca406bd24505044c18f2644aed29
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-28 15:45:52 +0100

    Cactus refactoring

--- cactusrenderer.cpp
+++ cactusrenderer.cpp
@@ -1,6 +1,6 @@
 #include "cactusrenderer.h"
 
-constexpr GLFix CactusRenderer::cactus_height, CactusRenderer::cactus_width;
+constexpr GLFix CactusRenderer::cactus_width;
 
 void CactusRenderer::renderSpecialBlock(const BLOCK_WDATA block, GLFix x, GLFix y, GLFix z, Chunk &c)
 {
@@ -9,13 +9,9 @@ void CactusRenderer::renderSpecialBlock(const BLOCK_WDATA block, GLFix x, GLFix 
 
     // Cactus offset, the offsettiness of the cactus (it isn't a full block ya know)
     const GLFix cactus_offset = (GLFix(BLOCK_SIZE) - cactus_width) * GLFix(0.5f);
-    const GLFix cactus_top_offset = BLOCK_SIZE;//(GLFix(BLOCK_SIZE) - cactus_width) * GLFix(0.5f);
 
     const TextureAtlasEntry &cactus_top = terrain_atlas[5][4].current;
     TextureAtlasEntry cactus_sid = terrain_atlas[6][4].current;
-
-    cactus_sid.top = cactus_sid.top + (cactus_sid.bottom - cactus_sid.top) * 9 / 16;
-
 
     //////
     // GL CODE
@@ -28,65 +24,36 @@ void CactusRenderer::renderSpecialBlock(const BLOCK_WDATA block, GLFix x, GLFix 
     std::vector<VERTEX> cactus_vertices;
     cactus_vertices.reserve(20);
 
-    //cactus_sid.right = cactus_sid.right - (cactus_sid.right - cactus_sid.left) * (cactus_max_bites - cactus_bites) / 6;
-
-    GLFix cactus_left_texturemap = cactus_sid.left;
-    GLFix cactus_right_texturemap = cactus_sid.right;
-
-    GLFix cactus_top_texturemap = cactus_top.right;
-
 
     // Cactus Side
     cactus_vertices.push_back({0, 0, GLFix(0) + cactus_offset, cactus_sid.left, cactus_sid.bottom, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({0, GLFix(0) + cactus_height, GLFix(0) + cactus_offset, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_width, GLFix(0) + cactus_height, GLFix(0) + cactus_offset, cactus_right_texturemap, cactus_sid.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_width, 0, GLFix(0) + cactus_offset, cactus_right_texturemap, cactus_sid.bottom, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({0, GLFix(0) + BLOCK_SIZE, GLFix(0) + cactus_offset, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_width, GLFix(0) + BLOCK_SIZE, GLFix(0) + cactus_offset, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_width, 0, GLFix(0) + cactus_offset, cactus_sid.right, cactus_sid.bottom, TEXTURE_TRANSPARENT});
 
     // Cactus Side
-    cactus_vertices.push_back({GLFix(0) + cactus_width, 0, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_left_texturemap, cactus_sid.bottom, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_width, GLFix(0) + cactus_height, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_left_texturemap, cactus_sid.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({0, GLFix(0) + cactus_height, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_width, 0, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_sid.left, cactus_sid.bottom, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_width, GLFix(0) + BLOCK_SIZE, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({0, GLFix(0) + BLOCK_SIZE, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
     cactus_vertices.push_back({0, 0, GLFix(0) - cactus_offset + BLOCK_SIZE, cactus_sid.right, cactus_sid.bottom, TEXTURE_TRANSPARENT});
 
     // Cactus Back Side
     cactus_vertices.push_back({GLFix(0) + cactus_offset, 0, GLFix(0) + BLOCK_SIZE, cactus_sid.left, cactus_sid.bottom, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_offset, GLFix(0) + cactus_height, 0 + BLOCK_SIZE, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_offset, GLFix(0) + cactus_height, 0, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_offset, GLFix(0) + BLOCK_SIZE, 0 + BLOCK_SIZE, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_offset, GLFix(0) + BLOCK_SIZE, 0, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
     cactus_vertices.push_back({GLFix(0) + cactus_offset, 0, 0, cactus_sid.right, cactus_sid.bottom, TEXTURE_TRANSPARENT});
 
     // Cactus (Front) Inside
     cactus_vertices.push_back({(GLFix(0) - cactus_offset) + cactus_width, 0, 0, cactus_sid.left, cactus_sid.bottom, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({(GLFix(0) - cactus_offset) + cactus_width, GLFix(0) + cactus_height, 0, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({(GLFix(0) - cactus_offset) + cactus_width, GLFix(0) + cactus_height, 0 + BLOCK_SIZE, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({(GLFix(0) - cactus_offset) + cactus_width, GLFix(0) + BLOCK_SIZE, 0, cactus_sid.left, cactus_sid.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({(GLFix(0) - cactus_offset) + cactus_width, GLFix(0) + BLOCK_SIZE, 0 + BLOCK_SIZE, cactus_sid.right, cactus_sid.top, TEXTURE_TRANSPARENT});
     cactus_vertices.push_back({(GLFix(0) - cactus_offset) + cactus_width, 0, 0 + BLOCK_SIZE, cactus_sid.right, cactus_sid.bottom, TEXTURE_TRANSPARENT});
 
     // Cactus Top
-    cactus_vertices.push_back({GLFix(0) + cactus_top_offset, GLFix(0) + cactus_height, GLFix(0) + cactus_top_offset, cactus_top.left, cactus_top.bottom, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_top_offset, GLFix(0) + cactus_height, GLFix(0) + BLOCK_SIZE - cactus_top_offset, cactus_top.left, cactus_top.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_width - cactus_top_offset, GLFix(0) + cactus_height, GLFix(0) + BLOCK_SIZE - cactus_top_offset, cactus_top_texturemap, cactus_top.top, TEXTURE_TRANSPARENT});
-    cactus_vertices.push_back({GLFix(0) + cactus_width - cactus_top_offset, GLFix(0) + cactus_height, GLFix(0) + cactus_top_offset, cactus_top_texturemap, cactus_top.bottom, TEXTURE_TRANSPARENT});
-
-    // Rotate Cactus According To Face
-    BLOCK_SIDE side = static_cast<BLOCK_SIDE>(getBLOCKDATA(block) & BLOCK_SIDE_BITS);
-
-
-    switch(side)
-    {
-        default:
-            break;
-        case BLOCK_BACK:
-            nglRotateY(270);
-            break;
-        case BLOCK_FRONT:
-            nglRotateY(90);
-            break;
-        case BLOCK_LEFT:
-            nglRotateY(180);
-            break;
-        case BLOCK_RIGHT:
-            nglRotateY(0);
-            break;
-    }
+    cactus_vertices.push_back({GLFix(0) + BLOCK_SIZE, GLFix(0) + BLOCK_SIZE, GLFix(0) + BLOCK_SIZE, cactus_top.left, cactus_top.bottom, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + BLOCK_SIZE, GLFix(0) + BLOCK_SIZE, GLFix(0) + BLOCK_SIZE - BLOCK_SIZE, cactus_top.left, cactus_top.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_width - BLOCK_SIZE, GLFix(0) + BLOCK_SIZE, GLFix(0) + BLOCK_SIZE - BLOCK_SIZE, cactus_top.right, cactus_top.top, TEXTURE_TRANSPARENT});
+    cactus_vertices.push_back({GLFix(0) + cactus_width - BLOCK_SIZE, GLFix(0) + BLOCK_SIZE, GLFix(0) + BLOCK_SIZE, cactus_top.right, cactus_top.bottom, TEXTURE_TRANSPARENT});
 
     glTranslatef(-BLOCK_SIZE / 2, -BLOCK_SIZE / 2, -BLOCK_SIZE / 2);
 
@@ -110,18 +77,16 @@ void CactusRenderer::geometryNormalBlock(const BLOCK_WDATA /*block*/, const int 
     renderNormalBlockSide(local_x, local_y, local_z, side, terrain_atlas[12][7].current, c);
 }
 
-AABB CactusRenderer::getAABB(const BLOCK_WDATA block, GLFix x, GLFix y, GLFix z)
+AABB CactusRenderer::getAABB(const BLOCK_WDATA /*block*/, GLFix x, GLFix y, GLFix z)
 {
-    // Get block side
-    BLOCK_SIDE side = static_cast<BLOCK_SIDE>(getBLOCKDATA(block) & BLOCK_SIDE_BITS);
     const GLFix cactus_offset = (GLFix(BLOCK_SIZE) - cactus_width) * GLFix(0.5f);
 
-    return {x + cactus_offset, y, z + cactus_offset, x + cactus_offset + cactus_width, y + cactus_height, z + cactus_offset + cactus_width};
+    return {x + cactus_offset, y, z + cactus_offset, x + cactus_offset + cactus_width, y + BLOCK_SIZE, z + cactus_offset + cactus_width};
 }
 
 void CactusRenderer::drawPreview(const BLOCK_WDATA /*block*/, TEXTURE &dest, int x, int y)
 {
-    TextureAtlasEntry &tex = terrain_atlas[7][4].resized;
+    TextureAtlasEntry &tex = terrain_atlas[6][4].resized;
     BlockRenderer::drawTextureAtlasEntry(*terrain_resized, tex, dest, x, y);
 }
 
