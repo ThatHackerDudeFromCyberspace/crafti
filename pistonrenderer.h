ed135d039410a697669afcd4a2bf9a7cd96658fb
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2022-07-04 10:25:17 +0100

    Fix piston backface culling
    - Fix piston backface culling

--- pistonrenderer.h
+++ pistonrenderer.h
@@ -25,6 +25,7 @@ public:
 protected:
     static constexpr GLFix piston_height = BLOCK_SIZE;
     static constexpr GLFix piston_width = BLOCK_SIZE;
+    const GLFix pistion_block_size = 0;
 
     /// Bitmap stuff
     static constexpr uint8_t piston_bit_shift = 4; // The amount to shift piston data by to give it room for the orientation or other additional data
