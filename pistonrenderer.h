f6031e41e5cc1ecdda5edefff2ba1e7e7c651550
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2022-07-04 19:28:37 +0100

    Add preliminary piston redstone logic
    - Add preliminary piston redstone logic

--- pistonrenderer.h
+++ pistonrenderer.h
@@ -13,10 +13,14 @@ public:
     virtual bool isOriented(const BLOCK_WDATA /*block*/) override { return true; } // Oriented
     virtual bool isFullyOriented(const BLOCK_WDATA /*block*/) override { return false; } // Torch-like orientation
 
+    virtual void tick(const BLOCK_WDATA block, int local_x, int local_y, int local_z, Chunk &c) override;
     virtual bool isBlockShaped(const BLOCK_WDATA /*block*/) override { return true; }
 
+    // Used for particles spawned on destruction
+    virtual const TerrainAtlasEntry &destructionTexture(const BLOCK_WDATA block) override;
+
     virtual void drawPreview(const BLOCK_WDATA block, TEXTURE &dest, int x, int y) override;
-    
+
     virtual const char* getName(const BLOCK_WDATA block) override;
 
 protected:
@@ -26,9 +30,16 @@ protected:
         PISTON_HEAD
     };
 
+    enum REDSTONE_STATE {
+        OFF=0,
+        ON
+    };
+
     /// Bitmap stuff
-    static constexpr uint8_t piston_bit_shift = 3; // The amount to shift piston data by to give it room for the orientation or other additional data
-    static constexpr uint8_t piston_data_bits = 0b11 << piston_bit_shift; // Piston uses 2 bits of data, however, orientation data is stored in the first three bits, so the piston data has to be shifted by 3 (or more)
+    static const uint8_t piston_bit_shift = 3; // The amount to shift piston data by to give it room for the orientation or other additional data
+    static const uint8_t piston_data_bits = 0b11 << piston_bit_shift; // Piston uses 2 bits of data, however, orientation data is stored in the first three bits, so the piston data has to be shifted by 3 (or more)
+    static const uint8_t piston_power_bit_shift = 5;
+    static const uint8_t piston_powered_bits = 0b1 << piston_power_bit_shift;
 };
 
 #endif // PISTONRENDERER_H
