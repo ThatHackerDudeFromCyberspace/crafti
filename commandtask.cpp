48385e4bbc6e74cbd88d615533341b48faec58de
Bluebotlabz <christtian.chan.g@gmail.com>
2022-07-08 18:28:20 +0100

    Update commandtask.cpp

--- commandtask.cpp
+++ commandtask.cpp
@@ -81,7 +81,7 @@ void CommandTask::render()
 void CommandTask::logic()
 {
     if(key_held_down)
-        key_held_down = keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_DIVIDE) || keyPressed(KEY_NSPIRE_A) || keyPressed(KEY_NSPIRE_B) || keyPressed(KEY_NSPIRE_C) || keyPressed(KEY_NSPIRE_D) || keyPressed(KEY_NSPIRE_E) || keyPressed(KEY_NSPIRE_F) || keyPressed(KEY_NSPIRE_G) || keyPressed(KEY_NSPIRE_H) || keyPressed(KEY_NSPIRE_I) || keyPressed(KEY_NSPIRE_J) || keyPressed(KEY_NSPIRE_K) || keyPressed(KEY_NSPIRE_L) || keyPressed(KEY_NSPIRE_M) || keyPressed(KEY_NSPIRE_N) || keyPressed(KEY_NSPIRE_O) || keyPressed(KEY_NSPIRE_P) || keyPressed(KEY_NSPIRE_Q) || keyPressed(KEY_NSPIRE_R) || keyPressed(KEY_NSPIRE_S) || keyPressed(KEY_NSPIRE_T) || keyPressed(KEY_NSPIRE_U) || keyPressed(KEY_NSPIRE_V) || keyPressed(KEY_NSPIRE_W) || keyPressed(KEY_NSPIRE_X) || keyPressed(KEY_NSPIRE_Y) || keyPressed(KEY_NSPIRE_Z) || keyPressed(KEY_NSPIRE_DEL) || keyPressed(KEY_NSPIRE_SPACE) || keyPressed(KEY_NSPIRE_0) || keyPressed(KEY_NSPIRE_1) || keyPressed(KEY_NSPIRE_2) || keyPressed(KEY_NSPIRE_3) || keyPressed(KEY_NSPIRE_4) || keyPressed(KEY_NSPIRE_5) || keyPressed(KEY_NSPIRE_6) || keyPressed(KEY_NSPIRE_7) || keyPressed(KEY_NSPIRE_8) || keyPressed(KEY_NSPIRE_9) || keyPressed(KEY_NSPIRE_NEGATIVE);
+        key_held_down = keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_DIVIDE) || keyPressed(KEY_NSPIRE_A) || keyPressed(KEY_NSPIRE_B) || keyPressed(KEY_NSPIRE_C) || keyPressed(KEY_NSPIRE_D) || keyPressed(KEY_NSPIRE_E) || keyPressed(KEY_NSPIRE_F) || keyPressed(KEY_NSPIRE_G) || keyPressed(KEY_NSPIRE_H) || keyPressed(KEY_NSPIRE_I) || keyPressed(KEY_NSPIRE_J) || keyPressed(KEY_NSPIRE_K) || keyPressed(KEY_NSPIRE_L) || keyPressed(KEY_NSPIRE_M) || keyPressed(KEY_NSPIRE_N) || keyPressed(KEY_NSPIRE_O) || keyPressed(KEY_NSPIRE_P) || keyPressed(KEY_NSPIRE_Q) || keyPressed(KEY_NSPIRE_R) || keyPressed(KEY_NSPIRE_S) || keyPressed(KEY_NSPIRE_T) || keyPressed(KEY_NSPIRE_U) || keyPressed(KEY_NSPIRE_V) || keyPressed(KEY_NSPIRE_W) || keyPressed(KEY_NSPIRE_X) || keyPressed(KEY_NSPIRE_Y) || keyPressed(KEY_NSPIRE_Z) || keyPressed(KEY_NSPIRE_DEL) || keyPressed(KEY_NSPIRE_SPACE) || keyPressed(KEY_NSPIRE_0) || keyPressed(KEY_NSPIRE_1) || keyPressed(KEY_NSPIRE_2) || keyPressed(KEY_NSPIRE_3) || keyPressed(KEY_NSPIRE_4) || keyPressed(KEY_NSPIRE_5) || keyPressed(KEY_NSPIRE_6) || keyPressed(KEY_NSPIRE_7) || keyPressed(KEY_NSPIRE_8) || keyPressed(KEY_NSPIRE_9) || keyPressed(KEY_NSPIRE_NEGATIVE)|| keyPressed(KEY_NSPIRE_PERIOD);
 
     else if (keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_DIVIDE)) {
         world_task.makeCurrent();
@@ -196,6 +196,9 @@ void CommandTask::logic()
         key_held_down = true;
     } else if (keyPressed(KEY_NSPIRE_NEGATIVE)) {
         command.push_back('-');
+        key_held_down = true;
+    } else if (keyPressed(KEY_NSPIRE_PERIOD)) {
+        command.push_back('.');
         key_held_down = true;
     } else if (keyPressed(KEY_NSPIRE_SPACE)) {
         command.push_back(' ');
