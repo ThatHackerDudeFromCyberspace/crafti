c8819c13983b678eb4fdcda8afc8b1d8a7c34667
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2022-07-08 09:44:22 +0100

    Update redstone block texture

--- redstoneblockrenderer.cpp
+++ redstoneblockrenderer.cpp
@@ -4,7 +4,7 @@
 
 void RedstoneBlockRenderer::geometryNormalBlock(const BLOCK_WDATA /*block*/, const int local_x, const int local_y, const int local_z, const BLOCK_SIDE side, Chunk &c)
 {
-    BlockRenderer::renderNormalBlockSide(local_x, local_y, local_z, side, terrain_atlas[4][13].current, c);
+    BlockRenderer::renderNormalBlockSide(local_x, local_y, local_z, side, terrain_atlas[10][1].current, c);
 }
 
 void RedstoneBlockRenderer::drawPreview(const BLOCK_WDATA /*block*/, TEXTURE &dest, int x, int y)
@@ -18,7 +18,7 @@ PowerState RedstoneBlockRenderer::powersSide(const BLOCK_WDATA /*block*/, BLOCK_
 }
 
 const TerrainAtlasEntry &RedstoneBlockRenderer::destructionTexture(const BLOCK_WDATA /*block*/) {
-    return terrain_atlas[4][13];
+    return terrain_atlas[10][1];
 }
 
 const char *RedstoneBlockRenderer::getName(const BLOCK_WDATA)
