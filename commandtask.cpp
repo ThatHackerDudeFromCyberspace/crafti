a2d98b33f5322b359e20166f08575e59598ed7d6
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2022-07-08 15:47:50 +0100

    Work on commands

--- commandtask.cpp
+++ commandtask.cpp
@@ -30,21 +30,8 @@ void CommandTask::render()
     const unsigned int x = (SCREEN_WIDTH - background->width) / 2;
     const unsigned int y = (SCREEN_HEIGHT - background->height) / 2;
     drawTextureOverlay(*background, 0, 0, *screen, x, y, background->width, background->height);
-    drawString("Commands for Crafti v1.4 [C14DB]", 0xFFFF, *screen, x, y - fontHeight());
 
-    drawString("8-4-6-2: Walk around\t5: Jump\n"
-               "7: Put block down   \t9: Destroy block\n"
-               "1-3: Change inventory slot\n"
-               "ESC: Save & Exit\n"
-               ".: Open list of blocks\n"
-               "    5: Change block in inventory\n"
-               "    . or ESC: Close list of blocks\n"
-               "Menu: Open menu\n"
-               "    2-8: Move cursor\t5: Select\n"
-               "Ctrl+.: Take screenshot\n"
-               "\n"
-               "Programmed by Fabian Vogt & Bluebotlaboratories\n"
-               "Textures from PureBDcraft by https://bdcraft.net", 0xFFFF, *screen, x + 10, y + 8);
+    drawString("Commands for Crafti v1.4 [C14DB]", 0xFFFF, *screen, x, y);
 }
 
 void CommandTask::logic()
